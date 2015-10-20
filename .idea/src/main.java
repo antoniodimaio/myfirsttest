3d44ca922da4ea3c61bdb43c69b7e6788da170c0
Antonio Di <adimaio@krk-mpn2x.krakow.corp.akamai.com>
2015-10-20 15:44:39 +0200

    3 test

--- .idea/src/main.java
+++ .idea/src/main.java
@@ -2,4 +2,9 @@
  * Created by adimaio on 10/20/15.
  */
 public class main {
+    public static void main(String [ ] args)
+    {
+        student antonio = new student();
+        System.out.println("this is test");
+    }
 }
--- .idea/src/student.java
+++ .idea/src/student.java
@@ -2,4 +2,8 @@
  * Created by adimaio on 10/20/15.
  */
 public class student {
+
+    private String name;
+    private String passaporto;
+    private String passaporto2;
 }
